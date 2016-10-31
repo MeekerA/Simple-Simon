f91143d15d0e54623a554bb289350ab6a7633728
Austin Meeker <austinmeeker@me.com>
2016-10-31 16:24:11 -0500

    Simple_Simon_Javascript - button click animation working. Setup Arrays to receive random number sequence.

--- JAVASCRIPT/simpleSimon_Javascript.js
+++ JAVASCRIPT/simpleSimon_Javascript.js
@@ -1,3 +1,51 @@
 /**
  * Created by Austin on 10/31/16.
  */
+
+"use strict";
+
+var gameColorBoxs = $(".gameColorBox");
+var oneRedBox = $("#redGameBox");
+var twoBlueBox = $("#blueGameBox");
+var threeGreenBox = $("#greenGameBox");
+var fourYellowBox = $("#yellowGameBox");
+
+var simpleSimonsInput = [];
+var playersInput = [];
+
+gameColorBoxs.click(function() {
+    // console.log($(this));
+    $(this).css({
+        opacity: "1"
+    }).animate({
+        opacity: ".2"
+    }, 400)
+});
+
+oneRedBox.click(function(){
+   playersInput.push(1);
+    console.log(playersInput);
+});twoBlueBox.click(function(){
+    playersInput.push(2);
+    console.log(playersInput);
+});threeGreenBox.click(function(){
+    playersInput.push(3);
+    console.log(playersInput);
+});fourYellowBox.click(function(){
+    playersInput.push(4);
+    console.log(playersInput);
+});
+
+
+for (var i = 0; i < 5; i++) {
+    var randomNumber = Math.floor((Math.random() * 4) + 1);
+    simpleSimonsInput.push(randomNumber)
+}
+console.log(simpleSimonsInput);
+
+
+// oneRedBox.animate({
+//     opacity: "1"
+// }, 300).animate({
+//     opacity: ".2"
+// }, 300);
