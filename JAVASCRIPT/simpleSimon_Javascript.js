679c6e24a4c02a912b14c2ca8012b642795653e9
Austin Meeker <austinmeeker@me.com>
2016-11-01 14:32:21 -0500

    Simple_Simon_Javascript -
    Combined computer button assignment switch and play back to one function.
    
    Simple Simon playback function to include HTML change on the page.

--- JAVASCRIPT/simpleSimon_Javascript.js
+++ JAVASCRIPT/simpleSimon_Javascript.js
@@ -9,87 +9,89 @@ var oneRedBox = $("#redGameBox");
 var twoBlueBox = $("#blueGameBox");
 var threeGreenBox = $("#greenGameBox");
 var fourYellowBox = $("#yellowGameBox");
+var gamesStartButton = $("#startGameButton");
+var buttonArea = $("#buttonArea");
+var gameInstructionText = $("#gameInstructionText");
 
 var simpleSimonsInput = [];
 var playersInput = [];
+var roundCounter = 1;
 
-function simpleSimonAnimate(intigerValue) {
-    switch (intigerValue) {
-        case 1:
-            oneRedBox.animate({
-                opacity: "1"
-            }, 300).animate({
-                opacity: ".2"
-            }, 300);
-            break;
-        case 2:
-            twoBlueBox.animate({
-                opacity: "1"
-            }, 300).animate({
-                opacity: ".2"
-            }, 300);
-            break;
-        case 3:
-            threeGreenBox.animate({
-                opacity: "1"
-            }, 300).animate({
-                opacity: ".2"
-            }, 300);
-            break;
-        case 4:
-            fourYellowBox.animate({
-                opacity: "1"
-            }, 300).animate({
-                opacity: ".2"
-            }, 300);
-    }
-}
-
-function showSimonsNumbers(arrayOfNumbers, timeBetweenNumbers) {
+function showSimonsNumbers(arrayOfNumbers, timeBetweenNumbers1000) {
     var i = 0;
+    gameInstructionText.html("Watch the sequence.");
     var SimpleSimonstimer = setInterval(function () {
-        simpleSimonAnimate(arrayOfNumbers[i]);
-        i++;
-        if (i >= simpleSimonsInput.length) {
-            clearInterval(SimpleSimonstimer)
+        switch (arrayOfNumbers[i]) {
+            case 1:
+                oneRedBox.animate({
+                    opacity: "1"
+                }, 300).animate({
+                    opacity: ".1"
+                }, 300);
+                break;
+            case 2:
+                twoBlueBox.animate({
+                    opacity: "1"
+                }, 300).animate({
+                    opacity: ".1"
+                }, 300);
+                break;
+            case 3:
+                threeGreenBox.animate({
+                    opacity: "1"
+                }, 300).animate({
+                    opacity: ".1"
+                }, 300);
+                break;
+            case 4:
+                fourYellowBox.animate({
+                    opacity: "1"
+                }, 300).animate({
+                    opacity: ".1"
+                }, 300);
         }
-    }, timeBetweenNumbers);
+        i++;
+        if (i >= arrayOfNumbers.length) {
+
+            clearInterval(SimpleSimonstimer);
+
+            setTimeout(function(){
+                gameInstructionText.html("Enter the sequence.")
+            }, 1000);
+        }
+    }, timeBetweenNumbers1000);
 }
 
 // -------------- variable and function declaration above ---------------
 
-gameColorBoxs.each(function (index, element) {
-    $(element).click(function () {
-        $(element).css({
-            opacity: "1"
-        }).animate({
-            opacity: ".2"
-        }, 400);
-        playersInput.push(index + 1);
-        console.log(playersInput);
+
+gamesStartButton.click(function () {
+
+    buttonArea.html("<h4>Round: " + roundCounter + "</h4>");
+    gameInstructionText.html("");
+
+    for (var i = 0; i < roundCounter + 1; i++) {
+        var randomNumber = Math.floor((Math.random() * 4) + 1);
+        simpleSimonsInput.push(randomNumber)
+    }
+    console.log(simpleSimonsInput);
+
+
+    showSimonsNumbers(simpleSimonsInput, 1000);
+
+    gameColorBoxs.each(function (index, element) {
+        $(element).click(function () {
+            $(element).css({
+                opacity: "1"
+            }).animate({
+                opacity: ".1"
+            }, 400);
+            playersInput.push(index + 1);
+            console.log(playersInput);
+        });
     });
+
+
 });
 
-for (var i = 0; i < 2; i++) {
-    var randomNumber = Math.floor((Math.random() * 4) + 1);
-    simpleSimonsInput.push(randomNumber)
-}
-console.log(simpleSimonsInput);
 
-showSimonsNumbers(simpleSimonsInput, 400);
-
-
-// oneRedBox.animate({
-//     opacity: "1"
-// }, 300).animate({
-//     opacity: ".2"
-// }, 300);
-
-// gameColorBoxs.click(function () {
-//     // console.log($(this));
-//     $(this).css({
-//         opacity: "1"
-//     }).animate({
-//         opacity: ".2"
-//     }, 400)
-// });
