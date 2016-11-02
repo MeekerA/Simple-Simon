486fa097fa8dc9a113be7eab90d62bad8c6f2dbd
Austin Meeker <austinmeeker@me.com>
2016-11-02 15:14:49 -0500

    Simple_Simon_Javascript -
    Game working, Added readme.md "no content yet", need to add restart button.

--- JAVASCRIPT/simpleSimon_Javascript.js
+++ JAVASCRIPT/simpleSimon_Javascript.js
@@ -16,6 +16,75 @@ var gameInstructionText = $("#gameInstructionText");
 var simpleSimonsInput = [];
 var playersInput = [];
 var roundCounter = 1;
+var gamePlaySpeed = 1000;
+var counter = 0;
+
+
+// ----------------- Variable declaration above --------------------
+
+
+function simonAnimatesBox(animationBox, speed300) {
+    animationBox.animate({
+        opacity: "1"
+    }, speed300).animate({
+        opacity: ".1"
+    }, speed300);
+}
+
+var dumbVariable = false;
+function userAnimatesBox() {
+    gameColorBoxs.each(function (index, element) {
+        $(element).click(function () {
+            if (dumbVariable == true) {
+                $(element).css({
+                    opacity: "1"
+                }).animate({
+                    opacity: ".1"
+                }, 400);
+                playersInput.push(index + 1);
+
+                if (playersInput[counter] == simpleSimonsInput[counter]) {
+                    counter++;
+                } else {
+                    // player lost
+                    setTimeout(function () {
+
+                        dumbVariable = false;
+
+                        gameInstructionText.html("Wrong Answer..");
+
+                        buttonArea.html("<h4>You Made it to Round: " + roundCounter + "</h4>");
+
+                    }, 400)
+                }
+
+                if (simpleSimonsInput.length == counter) {
+                    setTimeout(function () {
+
+                        roundCounter++;
+                        gamePlaySpeed -= 50;
+                        counter = 0;
+                        playersInput = [];
+
+                        buttonArea.html("<h4>Round: " + roundCounter + "</h4>");
+                        gameInstructionText.html("");
+
+                        var randomNumber = Math.floor((Math.random() * 4) + 1);
+                        simpleSimonsInput.push(randomNumber);
+
+                        console.log(simpleSimonsInput);
+
+                        dumbVariable = false;
+
+                        showSimonsNumbers(simpleSimonsInput, gamePlaySpeed);
+
+                    }, 400)
+                }
+            }
+        });
+    });
+}
+
 
 function showSimonsNumbers(arrayOfNumbers, timeBetweenNumbers1000) {
     var i = 0;
@@ -23,75 +92,73 @@ function showSimonsNumbers(arrayOfNumbers, timeBetweenNumbers1000) {
     var SimpleSimonstimer = setInterval(function () {
         switch (arrayOfNumbers[i]) {
             case 1:
-                oneRedBox.animate({
-                    opacity: "1"
-                }, 300).animate({
-                    opacity: ".1"
-                }, 300);
+                simonAnimatesBox(oneRedBox, 300);
                 break;
             case 2:
-                twoBlueBox.animate({
-                    opacity: "1"
-                }, 300).animate({
-                    opacity: ".1"
-                }, 300);
+                simonAnimatesBox(twoBlueBox, 300);
                 break;
             case 3:
-                threeGreenBox.animate({
-                    opacity: "1"
-                }, 300).animate({
-                    opacity: ".1"
-                }, 300);
+                simonAnimatesBox(threeGreenBox, 300);
                 break;
             case 4:
-                fourYellowBox.animate({
-                    opacity: "1"
-                }, 300).animate({
-                    opacity: ".1"
-                }, 300);
+                simonAnimatesBox(fourYellowBox, 300)
         }
         i++;
         if (i >= arrayOfNumbers.length) {
 
             clearInterval(SimpleSimonstimer);
 
-            setTimeout(function(){
-                gameInstructionText.html("Enter the sequence.")
+            setTimeout(function () {
+                gameInstructionText.html("Enter the sequence.");
+                dumbVariable = true;
             }, 1000);
         }
     }, timeBetweenNumbers1000);
 }
 
-// -------------- variable and function declaration above ---------------
 
+// -------------- Start Button on Page Load ---------------
 
 gamesStartButton.click(function () {
 
+    roundCounter = 1;
+
+    playersInput = [];
+    simpleSimonsInput = [];
     buttonArea.html("<h4>Round: " + roundCounter + "</h4>");
     gameInstructionText.html("");
 
-    for (var i = 0; i < roundCounter + 1; i++) {
-        var randomNumber = Math.floor((Math.random() * 4) + 1);
-        simpleSimonsInput.push(randomNumber)
-    }
+
+    var randomNumber = Math.floor((Math.random() * 4) + 1);
+    simpleSimonsInput.push(randomNumber);
+    randomNumber = Math.floor((Math.random() * 4) + 1);
+    simpleSimonsInput.push(randomNumber);
+
     console.log(simpleSimonsInput);
 
 
-    showSimonsNumbers(simpleSimonsInput, 1000);
+    showSimonsNumbers(simpleSimonsInput, gamePlaySpeed);
 
-    gameColorBoxs.each(function (index, element) {
-        $(element).click(function () {
-            $(element).css({
-                opacity: "1"
-            }).animate({
-                opacity: ".1"
-            }, 400);
-            playersInput.push(index + 1);
-            console.log(playersInput);
-        });
-    });
-
+    userAnimatesBox();
 
 });
 
+// $("#restart").click(function() {
+//
+//     roundCounter = 1;
+//
+//     playersInput = [];
+//     simpleSimonsInput = [];
+//     buttonArea.html("<h4>Round: " + roundCounter + "</h4>");
+//     gameInstructionText.html("");
+//
+//     for (var i = 0; i < roundCounter + 1; i++) {
+//         var randomNumber = Math.floor((Math.random() * 4) + 1);
+//         simpleSimonsInput.push(randomNumber);
+//     }
+//     console.log(simpleSimonsInput);
+//
+//     showSimonsNumbers(simpleSimonsInput, gamePlaySpeed);
+//
+// });
 
