f6d1b5de9841eb4559cde04beac99eb414f52c15
Austin Meeker <austinmeeker@me.com>
2016-11-02 16:59:08 -0500

    Simple_Simon_Javascript -
    Added comments to Javascript, still need to add game restart button.

--- JAVASCRIPT/simpleSimon_Javascript.js
+++ JAVASCRIPT/simpleSimon_Javascript.js
@@ -4,26 +4,26 @@
 
 "use strict";
 
-var gameColorBoxs = $(".gameColorBox");
-var oneRedBox = $("#redGameBox");
-var twoBlueBox = $("#blueGameBox");
-var threeGreenBox = $("#greenGameBox");
-var fourYellowBox = $("#yellowGameBox");
-var gamesStartButton = $("#startGameButton");
-var buttonArea = $("#buttonArea");
+var gameColorBoxs = $(".gameColorBox"); // all games color boxes
+var oneRedBox = $("#redGameBox"); // game color box
+var twoBlueBox = $("#blueGameBox"); // blue game color box
+var threeGreenBox = $("#greenGameBox"); //green game color box
+var fourYellowBox = $("#yellowGameBox"); //yellow game color box
+var gamesStartButton = $("#startGameButton"); // "Start New Game" button
+var buttonArea = $("#buttonArea"); // container that
 var gameInstructionText = $("#gameInstructionText");
 
 var simpleSimonsInput = [];
 var playersInput = [];
 var roundCounter = 1;
-var gamePlaySpeed = 1000;
+var gamePlaySpeed = 1000; // games play speed // 1 sec between computers animation
 var counter = 0;
 
 
 // ----------------- Variable declaration above --------------------
 
 
-function simonAnimatesBox(animationBox, speed300) {
+function simonAnimatesBox(animationBox, speed300) { // animation for computer playback.
     animationBox.animate({
         opacity: "1"
     }, speed300).animate({
@@ -31,52 +31,63 @@ function simonAnimatesBox(animationBox, speed300) {
     }, speed300);
 }
 
-var dumbVariable = false;
-function userAnimatesBox() {
+function flashAllColorBoxes (animationBox) { // animation for when player loses.
+    animationBox.animate({
+        opacity: "1"
+    }, 300).animate({
+        opacity: ".1"
+    }, 300);
+}
+
+var turnClickCheckingOn = false; // variable that can be turned on and off to control when player can click boxes
+function userAnimatesBox() { // controls players box animation // checks player choice against games sequence
     gameColorBoxs.each(function (index, element) {
         $(element).click(function () {
-            if (dumbVariable == true) {
-                $(element).css({
+            if (turnClickCheckingOn == true) {
+                $(element).css({ // user animation on click
                     opacity: "1"
                 }).animate({
                     opacity: ".1"
                 }, 400);
-                playersInput.push(index + 1);
+                playersInput.push(index + 1); // red = 1 // blue = 2 // green = 3 // yellow = 4
 
-                if (playersInput[counter] == simpleSimonsInput[counter]) {
+                if (playersInput[counter] == simpleSimonsInput[counter]) { // checks players choice against games sequence
                     counter++;
-                } else {
-                    // player lost
-                    setTimeout(function () {
+                } else {    // player lost need to add restart button !!
 
-                        dumbVariable = false;
 
-                        gameInstructionText.html("Wrong Answer..");
+                    setTimeout(function () { // 1 second delay to finish players animation
+
+                        turnClickCheckingOn = false; // turns player clicking off
+
+                    flashAllColorBoxes(gameColorBoxs); // flashes all color boxes
+
+                        gameInstructionText.html("That wasn't right..");
 
                         buttonArea.html("<h4>You Made it to Round: " + roundCounter + "</h4>");
 
                     }, 400)
                 }
 
-                if (simpleSimonsInput.length == counter) {
-                    setTimeout(function () {
+                if (simpleSimonsInput.length == counter) { // if players sequence = computers sequence
+                    setTimeout(function () { // adds .4 sec delay to finish players animation
 
-                        roundCounter++;
-                        gamePlaySpeed -= 50;
-                        counter = 0;
-                        playersInput = [];
+                        roundCounter++; // adds one to round counter
+                        gamePlaySpeed -= 50; // reduces time between computer box animation
+                        counter = 0; // resets correct sequence counter to "0"
+                        playersInput = []; // clears players input
 
                         buttonArea.html("<h4>Round: " + roundCounter + "</h4>");
                         gameInstructionText.html("");
 
                         var randomNumber = Math.floor((Math.random() * 4) + 1);
-                        simpleSimonsInput.push(randomNumber);
+                        simpleSimonsInput.push(randomNumber); // adds random number to games sequence
 
-                        console.log(simpleSimonsInput);
+                        // console.log(simpleSimonsInput);
 
-                        dumbVariable = false;
+                        turnClickCheckingOn = false; // turns clicking off
 
-                        showSimonsNumbers(simpleSimonsInput, gamePlaySpeed);
+                        showSimonsNumbers(simpleSimonsInput, gamePlaySpeed); // runs next level
 
                     }, 400)
                 }
@@ -86,10 +97,10 @@ function userAnimatesBox() {
 }
 
 
-function showSimonsNumbers(arrayOfNumbers, timeBetweenNumbers1000) {
-    var i = 0;
+function showSimonsNumbers(arrayOfNumbers, timeBetweenNumbers1000) { // function shows player the computers sequence
+    var i = 0; // variable increases for arrayOfNumbers[i] to move threw the array
     gameInstructionText.html("Watch the sequence.");
-    var SimpleSimonstimer = setInterval(function () {
+    var SimpleSimonstimer = setInterval(function () { //setInterval controls the time between computer animation "Game Speed"
         switch (arrayOfNumbers[i]) {
             case 1:
                 simonAnimatesBox(oneRedBox, 300);
@@ -104,13 +115,14 @@ function showSimonsNumbers(arrayOfNumbers, timeBetweenNumbers1000) {
                 simonAnimatesBox(fourYellowBox, 300)
         }
         i++;
-        if (i >= arrayOfNumbers.length) {
+        if (i >= arrayOfNumbers.length) { // if statement that i == the array of numbers length
 
-            clearInterval(SimpleSimonstimer);
+            clearInterval(SimpleSimonstimer); // stops the setInterval
 
-            setTimeout(function () {
-                gameInstructionText.html("Enter the sequence.");
-                dumbVariable = true;
+            setTimeout(function () { // adds .1 sec delay to finish computers animation
+                gameInstructionText.html("Enter the sequence."); // changes text
+                turnClickCheckingOn = true; // turns clicking function on
+
             }, 1000);
         }
     }, timeBetweenNumbers1000);
@@ -133,9 +145,6 @@ gamesStartButton.click(function () {
     simpleSimonsInput.push(randomNumber);
     randomNumber = Math.floor((Math.random() * 4) + 1);
     simpleSimonsInput.push(randomNumber);
-
-    console.log(simpleSimonsInput);
-
 
     showSimonsNumbers(simpleSimonsInput, gamePlaySpeed);
 
---
+++ readme.md
@@ -0,0 +1,3 @@
+
+Simple Simon Game Project.
+
